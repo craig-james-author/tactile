f536417f65039501ed322dddd1be98c6070f5248
Craig James <craig.james.author@gmail.com>
2022-10-17 10:33:51 -0700

    Changed license terms to use LGPL v3 instead.

--- AudioPlaySdWavPR.cpp
+++ AudioPlaySdWavPR.cpp
@@ -1,3 +1,23 @@
+/* -*-C-*-
++======================================================================
+| Copyright (c) 2022, Craig A. James
+|
+| This file is part of of the "Tactile" library.
+|
+| Tactile is free software: you can redistribute it and/or modify it under
+| the terms of the GNU Lesser General Public License (LGPL) as published by
+| the Free Software Foundation, either version 3 of the License, or (at
+| your option) any later version.
+|
+| Tactile is distributed in the hope that it will be useful, but WITHOUT
+| ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+| FITNESS FOR A PARTICULAR PURPOSE. See the LGPL for more details.
+|
+| You should have received a copy of the LGPL along with Tactile. If not,
+| see <https://www.gnu.org/licenses/>.
++======================================================================
+*/
+
 #include <AudioPlaySdWavPR.h>
 
 void AudioPlaySdWavPR::update(void) {
--- TactileSensors.cpp
+++ TactileSensors.cpp
@@ -1,26 +1,20 @@
-/* -*-C++-*-
+/* -*-C-*-
 +======================================================================
-| Copyright (c) 2021, Craig A. James
+| Copyright (c) 2022, Craig A. James
 |
-| Permission is hereby granted, free of charge, to any person obtaining a
-| copy of this software and associated documentation files (the
-| "Software"), to deal in the Software without restriction, including
-| without limitation the rights to use, copy, modify, merge, publish,
-| distribute, sublicense, and/or sell copies of the Software, and to permit
-| persons to whom the Software is furnished to do so, subject to the
-| following conditions:
+| This file is part of of the "Tactile" library.
 |
-| The above copyright notice and this permission notice shall be included
-| in all copies or substantial portions of the Software.
+| Tactile is free software: you can redistribute it and/or modify it under
+| the terms of the GNU Lesser General Public License (LGPL) as published by
+| the Free Software Foundation, either version 3 of the License, or (at
+| your option) any later version.
 |
-| THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-| OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-| MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-| NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-| DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-| OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-| USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+| Tactile is distributed in the hope that it will be useful, but WITHOUT
+| ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+| FITNESS FOR A PARTICULAR PURPOSE. See the LGPL for more details.
+|
+| You should have received a copy of the LGPL along with Tactile. If not,
+| see <https://www.gnu.org/licenses/>.
 +======================================================================
 */
 
